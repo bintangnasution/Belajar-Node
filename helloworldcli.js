10d97b24e2938ac04558bfbfd8b6eb8e076b7f9d
Bintang Nasution <sumbercodeku@gmail.com>
2022-12-22 15:45:10 +0700

    first commit

---
+++ helloworldcli.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var username = process.argv[2];
+
+if (!username) {
+  var appName= process.argv[1].split(require('path').sep).pop();
+  console.error('Missing argument! Example: %s YOUR_NAME',appName);
+  process.exit(1);
+}
+
+console.log('Hello %s', username);
